cb95ff9b54b4ac01df1f5e0b3365e7e047d169e4
moshfiqrony <moshfiq.dev@gmail.com>
2024-02-21 19:28:57 +0600

    Add portal like behavior rnd

---
+++ src/components/common/Buttons.jsx
@@ -0,0 +1,33 @@
+import { forwardRef } from 'react';
+
+export const PrimaryButton = forwardRef(
+    /**
+     * @param {React.HTMLProps<HTMLButtonElement>} props
+     * @returns {JSX.Element}
+     */
+    ({ children, ...props }, ref) => {
+        return (
+            <button
+                ref={ref}
+                className="cursor-pointer bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-md"
+                {...props}
+            >
+                {children}
+            </button>
+        );
+    }
+);
+
+export const TextButton = forwardRef(
+    /**
+     * @param {React.HTMLProps<HTMLButtonElement>} props
+     * @returns {JSX.Element}
+     */
+    ({ children, ...props }, ref) => {
+        return (
+            <button ref={ref} className="cursor-pointer px-4 py-2 " {...props}>
+                {children}
+            </button>
+        );
+    }
+);
--- src/pages/OpeningPortalAnimation.jsx
+++ src/pages/OpeningPortalAnimation.jsx
@@ -1,9 +1,80 @@
 import { motion } from 'framer-motion';
+import { useState } from 'react';
+import { PrimaryButton, TextButton } from '../components/common/Buttons';
 
 export default function OpeningPortalAnimation() {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
-        <motion.div className="cursor-pointer">
-            Opening Portal Animation
-        </motion.div>
+        <div className="p-4 flex items-center justify-center h-screen">
+            <PrimaryButton onClick={() => setIsOpen(true)}>
+                Opening portal animation
+            </PrimaryButton>
+
+            {isOpen && (
+                <motion.div
+                    initial={{
+                        opacity: 0,
+                        scale: 0,
+                        x: '-50%',
+                        y: '-50%',
+                    }}
+                    animate={{
+                        opacity: 1,
+                        scale: 1,
+                        x: 0,
+                        y: 0,
+                    }}
+                    transition={{
+                        ease: 'easeInOut',
+                        originX: 0,
+                        originY: 0,
+                    }}
+                    className="absolute flex-col flex items-center justify-center top-0 left-0 z-[999999] max-h-screen h-screen w-full bg-white"
+                >
+                    <div className="max-w-[512px] flex gap-4 flex-col">
+                        <p className="text-lg font-medium">
+                            What is Lorem Ipsum?
+                        </p>
+                        <p>
+                            Lorem Ipsum is simply dummy text of the printing and
+                            typesetting industry. Lorem Ipsum has been the
+                            industry's standard dummy text ever since the 1500s,
+                            when an unknown printer took a galley of type and
+                            scrambled it to make a type specimen book. It has
+                            survived not only five centuries, but also the leap
+                            into electronic typesetting, remaining essentially
+                            unchanged. It was popularised in the 1960s with the
+                            release of Letraset sheets containing Lorem Ipsum
+                            passages, and more recently with desktop publishing
+                            software like Aldus PageMaker including versions of
+                            Lorem Ipsum.
+                        </p>
+
+                        <p className="text-lg font-medium mt-3">
+                            Why do we use it?
+                        </p>
+                        <p>
+                            It is a long established fact that a reader will be
+                            distracted by the readable content of a page when
+                            looking at its layout. The point of using Lorem
+                            Ipsum is that it has a more-or-less normal
+                            distribution of letters, as opposed to using
+                            'Content here, content here', making it look like
+                            readable English. Many desktop publishing packages
+                            and web page editors now use Lorem Ipsum as their
+                            default model text, and a search for 'lorem ipsum'
+                            will uncover many web sites still in their infancy.
+                            Various versions have evolved over the years,
+                            sometimes by accident, sometimes on purpose
+                            (injected humour and the like).
+                        </p>
+                    </div>
+                    <TextButton onClick={() => setIsOpen(false)}>
+                        Close
+                    </TextButton>
+                </motion.div>
+            )}
+        </div>
     );
 }
