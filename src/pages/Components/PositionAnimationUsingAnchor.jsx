429154e88eb97137c1f65299af1ddf23dca08d9f
moshfiqrony <moshfiq.dev@gmail.com>
2024-02-23 10:21:29 +0600

    Refactor PositionAnimationUsingAnchor component

--- src/pages/Components/PositionAnimationUsingAnchor.jsx
+++ src/pages/Components/PositionAnimationUsingAnchor.jsx
@@ -40,37 +40,41 @@ export default function PositionAnimationUsingAnchor() {
             header="Position animation using anchor"
             description="A simple position animation using anchor element, framer-motion and useDisclosure hook. This procedure can be also helpful for creating a portal animation."
         >
-            <Section id="example" heading="Example">
-                <div className="p-8">
-                    <PrimaryButton
-                        onClick={() => (isOpen ? handleClose() : handleOpen())}
+            <div className="flex gap-4 flex-col">
+                <Section id="source-code" heading="Source code">
+                    <ExternalLink
+                        href={`${REPO_URL}/pages/Components/PositionAnimationUsingAnchor.jsx`}
                     >
-                        {isOpen ? 'Reset' : 'Move'}
-                    </PrimaryButton>
+                        PositionAnimationUsingAnchor.jsx
+                    </ExternalLink>
+                </Section>
 
-                    <div ref={anchorRef} />
-                    <div
-                        {...(isAlive && {
-                            style: {
-                                position: 'absolute',
-                                top: anchorRect?.top,
-                                left: anchorRect?.left,
-                            },
-                        })}
-                        ref={ref}
-                    >
-                        <div className="w-[200px] h-[200px] bg-sky-300 mt-6"></div>
+                <Section id="example" heading="Example">
+                    <div className="p-8">
+                        <PrimaryButton
+                            onClick={() =>
+                                isOpen ? handleClose() : handleOpen()
+                            }
+                        >
+                            {isOpen ? 'Reset' : 'Move'}
+                        </PrimaryButton>
+
+                        <div ref={anchorRef} />
+                        <div
+                            {...(isAlive && {
+                                style: {
+                                    position: 'absolute',
+                                    top: anchorRect?.top,
+                                    left: anchorRect?.left,
+                                },
+                            })}
+                            ref={ref}
+                        >
+                            <div className="w-[200px] h-[200px] bg-sky-300 mt-6"></div>
+                        </div>
                     </div>
-                </div>
-            </Section>
-
-            <Section id="source-code" heading="Source code">
-                <ExternalLink
-                    href={`${REPO_URL}/pages/Components/PositionAnimationUsingAnchor.jsx`}
-                >
-                    PositionAnimationUsingAnchor.jsx
-                </ExternalLink>
-            </Section>
+                </Section>
+            </div>
         </Layout>
     );
 }
